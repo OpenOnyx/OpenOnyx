f4bb487966212b2886f248bf68185852b012f369
rishi-jat <rishijat098@gmail.com>
2026-07-29 19:08:36 +0530

    feat(ui): default to IBM Plex Sans and Mono for new installs
    Add fontsource packages, load them at startup, and keep Inter as a
    user-selectable interface font option.

--- src/main.tsx
+++ src/main.tsx
@@ -12,6 +12,13 @@ import { installGlobalTooltips } from "./lib/tooltips";
 import { documentTailwindClasses } from "./styles/documentTailwindClasses";
 import { themeClasses } from "./styles/themeClasses";
 
+import "@fontsource/ibm-plex-sans/300.css";
+import "@fontsource/ibm-plex-sans/400.css";
+import "@fontsource/ibm-plex-sans/500.css";
+import "@fontsource/ibm-plex-sans/600.css";
+import "@fontsource/ibm-plex-sans/700.css";
+import "@fontsource/ibm-plex-mono/400.css";
+import "@fontsource/ibm-plex-mono/500.css";
 import "@fontsource/inter/300.css";
 import "@fontsource/inter/400.css";
 import "@fontsource/inter/500.css";
